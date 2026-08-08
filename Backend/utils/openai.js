038b79ca01ac7e8236ba232966fdc60ecd31caef
Subhrajit Sahoo <subhrajits370@gmail.com>
2026-08-08 19:17:11 +0530

    Initial commit - SigmaGPT

---
+++ Backend/utils/openai.js
@@ -0,0 +1,28 @@
+import "dotenv/config";
+
+const getOpenAIAPIResponse = async (message) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${process.env.MISTRAL_API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "mistral-small-latest",
+            messages: [{
+                role: "user",
+                content: message
+            }]
+        })
+    };
+
+    try {
+        const response = await fetch("https://api.mistral.ai/v1/chat/completions", options);
+        const data = await response.json();
+        return data.choices[0].message.content; //reply
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export default getOpenAIAPIResponse;
